432541f064544be18fe67c6e9027479881676653
RockyJames <jaume@campeny.net>
2019-05-05 21:08:47 +0200

    #2 FEAT: Added upload_directory path and db parameters to the container

--- config/dependencies.php
+++ config/dependencies.php
@@ -6,8 +6,13 @@ use Slim\Http\Uri;
 use Slim\Views\Twig;
 use Slim\Views\TwigExtension;
 use \SallePW\SlimApp\Model\Product;
+use SallePW\SlimApp\Model\Database\Database;
+use SallePW\SlimApp\Model\Database\UserRepository;
+use Slim\Container;
 
 $container = $app->getContainer();
+
+$container['upload_directory'] = __DIR__ . '/../uploads';
 
 $container['view'] = function ($c) {
     $view = new Twig(__DIR__ . '/../templates', [
@@ -26,6 +31,20 @@ $container['view'] = function ($c) {
 $container['flash'] = function () {
     return new Messages();
 };
+
+$container['db'] = function (Container $c) {
+    return Database::getInstance(
+        $c['settings']['db']['username'],
+        $c['settings']['db']['password'],
+        $c['settings']['db']['host'],
+        $c['settings']['db']['dbName']
+    );
+};
+
+$container['user_repo'] = function (Container $c) {
+    return new UserRepository($c->get('db'));
+};
+
 
 $container['search'] = function () {
     $a = [new Product("iPhone 7", "Telefono semi-nuevo 32GB", "435", [],"Computers and Electronics" ), new Product("MacBook Air", "13 pulgadas 2011", "735", [],"Computers and Electronics" )];
