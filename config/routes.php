54396936f76d6a9528c05e8f8eedb48296c3fa49
RockyJames <jaume@campeny.net>
2019-05-05 21:09:30 +0200

    CHORE: Added /register routes

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,7 @@ use SallePW\SlimApp\Controller\FlashController;
 use SallePW\SlimApp\Controller\HelloController;
 use SallePW\SlimApp\Controller\SearchController;
 use SallePW\SlimApp\Controller\UploadController;
+use SallePW\SlimApp\Controller\RegisterController;
 use SallePW\SlimApp\Controller\Middleware\TestMiddleware;
 use SallePW\SlimApp\Controller\Middleware\SessionMiddleware;
 
@@ -18,6 +19,8 @@ $app->add(SessionMiddleware::class);
 $app->get('/home', SearchController::class)
     ->add(SearchController::class);
 
+$app->get('/register',RegisterController::class);
+$app->post('/register',RegisterController::class);
 
 $app->get('/upload', UploadController::class)
     ->add(UploadController::class);
