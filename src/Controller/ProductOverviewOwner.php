54dfd9b12663eb73e798fe525e13b31e0bc87760
GMO <palacin.guillermo@gmail.com>
2019-05-17 11:38:50 +0200

    Product Overview

---
+++ src/Controller/ProductOverviewOwner.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MB
+ * Date: 2019-04-18
+ * Time: 12:39
+ */
+
+namespace SallePW\SlimApp\Controller;
+
+use Dflydev\FigCookies\FigRequestCookies;
+use Dflydev\FigCookies\FigResponseCookies;
+use Dflydev\FigCookies\SetCookie;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use SallePW\SlimApp\Model\Product;
+
+
+final class ProductOverviewOwner {
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function __invoke(Request $request, Response $response)
+    {
+        $prod_id = $_POST["prod_id"];
+
+        $repository = $this->container->get('user_repo');
+
+        $products = $repository->getProductsFromDDBBbyID($prod_id);
+        $images = $repository->getImagesOfProductById();
+
+
+        return $this->container->get('view')->render($response, 'overviewowner.twig',[
+
+            'product' => $products,
+            'images' => $images,
+
+
+        ]);
+
+    }
+
+    public function checkProductCategory($products) {
+        $p = [];
+
+        for ($i = 0; $i < count($products); $i++) {
+            $prod = $products[$i];
+
+            switch ($prod['category']){
+                case 0:
+                    $prod[$i] = "Sports";
+                    break;
+                case 1:
+                    $prod[$i] = "Fashion";
+                    break;
+                case 2:
+                    $prod[$i] = "Computers and electronic";
+                    break;
+                case 3:
+                    $prod[$i] = "Cars";
+                    break;
+                case 4:
+                    $prod[$i] ="Games";
+                    break;
+                case 5:
+                    $prod[$i] = "Home";
+                    break;
+                case 6:
+                    $prod[$i] = "Other";
+                    break;
+
+
+            }
+        }
+        return $p;
+
+    }
+
+
+
+
+}
